d6f5fc4065c2ab11414629b690c72751040392d9
Sai Manoj <saimanoj2826@gmail.com>
2023-02-07 23:26:48 +0530

    added filter to the home page

---
+++ src/components/Filter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../css/Form.css';
+
+function Filter({ searchCountries, filterCountries }) {
+  const [input, setInput] = useState('');
+  const [region, setRegion] = useState('');
+
+  const handleInputChange = (evt) => {
+    setInput(evt.target.value);
+    searchCountries(input);
+  };
+
+  // FIXME: region is not updating properly
+  const handleFilterChange = (evt) => {
+    setRegion(evt.target.value);
+    filterCountries(evt.target.value);
+  };
+
+  return (
+    <div>
+      <form className='Form rounded-md bg-[#2B3945] p-4'>
+        <i className='fa-solid fa-magnifying-glass p-4 text-[30px] font-extrabold' />
+        <input type='text' value={input} onChange={handleInputChange} placeholder='Search for a country...' />
+      </form>
+      <select value={region} onChange={handleFilterChange}>
+        <option>Filter By Region</option>
+        <option>Asia</option>
+        <option>Africa</option>
+        <option>Europe</option>
+        <option>Americas</option>
+      </select>
+    </div>
+
+  );
+}
+
+export default Filter;
